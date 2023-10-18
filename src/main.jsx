c253240cdb6f06defb5e0cb5836b682316cf6062
mdrahathossenantor <md.rahathossenantor@gmail.com>
2023-10-18 19:25:56 +0600

    Initial project setup completed

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { RouterProvider } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import router from "./routes/Routes";
+import React from "react";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
